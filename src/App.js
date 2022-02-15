dea6ce575d7d9ed8047799e467731fa366f2402c
abc <abc@MacBook-Pro.local>
2022-02-14 23:25:48 -0600

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+const JacketsPage = () => (
+  <div>
+    <h1>Jackets Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={HatsPage}/>
+      <Route exact path='/shop/jackets' component={JacketsPage}/>
+    </Switch>
     </div>
   );
 }
